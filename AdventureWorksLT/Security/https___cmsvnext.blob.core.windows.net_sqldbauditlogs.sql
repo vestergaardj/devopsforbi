126e6c87bda730749ef4e87768d827afce68c87e
Jens Vestergaard <jve@catmansolution.com>
2022-08-28 09:11:10 +0200

    Add: AdventureWorksLT

---
+++ AdventureWorksLT/Security/https___cmsvnext.blob.core.windows.net_sqldbauditlogs.sql
@@ -0,0 +1,6 @@
+﻿CREATE MASTER KEY ENCRYPTION BY PASSWORD = 'FellowmindStyrerForVildt2022';
+GO;
+
+CREATE DATABASE SCOPED CREDENTIAL [https://cmsvnext.blob.core.windows.net/sqldbauditlogs]
+    WITH IDENTITY = N'SHARED ACCESS SIGNATURE';
+
